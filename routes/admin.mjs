f813bda8171463618067f5861d2abab9791d784c
John McCants <johnmccants002@gmail.com>
2024-08-28 20:34:43 -0700

    Updated the accept member route. Changed the field to membership status and created a deny member route

--- routes/admin.mjs
+++ routes/admin.mjs
@@ -17,7 +17,7 @@ router.put("/accept/:userId", async (req, res) => {
 
     const result = await usersCollection.updateOne(
       { _id: new ObjectId(userId) },
-      { $set: { isMember: true, updatedAt: new Date() } }
+      { $set: { membershipStatus: "accepted", updatedAt: new Date() } }
     );
 
     if (result.matchedCount === 0) {
@@ -27,6 +27,34 @@ router.put("/accept/:userId", async (req, res) => {
     return res
       .status(200)
       .json({ message: "User accepted as a member successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.put("/deny/:userId", async (req, res) => {
+  const { userId } = req.params;
+
+  if (!ObjectId.isValid(userId)) {
+    return res.status(400).json({ message: "Invalid user ID" });
+  }
+
+  try {
+    const usersCollection = await db.collection("users");
+
+    const result = await usersCollection.updateOne(
+      { _id: new ObjectId(userId) },
+      { $set: { membershipStatus: "denied", updatedAt: new Date() } }
+    );
+
+    if (result.matchedCount === 0) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "User denied membership successfully" });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Internal server error" });
